def7e14ae11eab16ab9342131fd8cb1899920419
ayeshanoor <ayeshaanoor11@gmail.com>
2025-09-29 01:32:02 +0500

    Add UI to Display Employee List

---
+++ employee_access_simulator/lib/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../data/employees.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Employee Access Simulator')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text("Employees List",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            Expanded(
+              child: ListView.builder(
+                itemCount: employees.length,
+                itemBuilder: (context, index) {
+                  final emp = employees[index];
+                  return ListTile(
+                    title: Text(emp.id),
+                    subtitle: Text(
+                        'Level: ${emp.accessLevel}, Room: ${emp.room}, Time: ${emp.requestTime}'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
