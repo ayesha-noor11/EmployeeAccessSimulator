ea92f0ec49be467b11394c7ea7e6876ddd8dbefd
ayeshanoor <ayeshaanoor11@gmail.com>
2025-09-28 22:56:11 +0500

    Initial Flutter project setup

---
+++ employee_access_simulator/android/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application") apply false
+    // Do NOT specify Kotlin version here; Flutter manages it
+    id("org.jetbrains.kotlin.android") apply false
+}
+
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
